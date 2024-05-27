17fbb4e41203b1cf6cd08c3a5a281a7dcbc279db
park3r0vsky <spenser96@o2.pl>
2024-05-27 02:18:29 +0200

    Added possibility to connect to postgres

--- backend/base/settings.py
+++ backend/base/settings.py
@@ -88,6 +88,18 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'yourdbname',
+#         'USER': 'yourdbuser',
+#         'PASSWORD': 'yourdbpassword',  
+#         'HOST': 'db', 
+#         'PORT': '5432',
+#     }
+# }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
